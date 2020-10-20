168764253797e66b4996199f170388c3017487a7
Lukas <24776816+lukasjankus@users.noreply.github.com>
2020-10-20 10:15:03 +0100

    Update Week2Task3.js

--- Week2Task3.js
+++ Week2Task3.js
@@ -1,3 +1,5 @@
+//Create a Person class that stores a person’s name and gender. Create a list of at least 10 Person objects. 
+//Write a function that displays all the people in the list of the same gender.
 class Person {
     constructor(name, gender) {
         this.name = name;
